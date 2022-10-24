2f7ce9813242a3d27897b01dc6a61b1ac51ea4c0
Loc <abc@gmail.com>
2022-10-24 18:25:50 +0700

    vong lap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,87 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+//        String name = "Phan Thanh Loc";
+//        int age = 29;
+//        String hobby = "football";
+//        Float tall = 1.65f;
+//
+//        System.out.println(name);
+//        System.out.println(age);
+//        System.out.println(hobby);
+//        System.out.println(tall);
+
+//        3: Toán tử
+
+//        int c = 10;
+//        int d = c++;
+
+//        toán tử ++
+//        case1:a++
+//        1: gán giá trị vào cho a
+//        2: tăng a lên 1 đơn vị
+//        case2:++a (ngược lại case1)
+
+
+//        int a = 5;
+//        int b = 5;
+//        int result = a++ - --b + b-- + a-- - a++ + b++ - b--;
+
+//        5 - - --b + b-- + a-- - a++ + b++ - b--
+//        5 - 4 + b-- + a-- - a++ + b++ - b--
+//        5 - 4 + 4 + a-- - a++ + b++ - b--
+//        5 - 4 + 4 + 6 - a++ + b++ - b--
+//        5 - 4 + 4 + 6 - 5 + b++ - b--
+//        5 - 4 + 4 + 6 - 5 + 3 - b--
+//        5 - 4 + 4 + 6 - 5 + 3 - 4
+//        a = 6, b = 3, result = 5;
+
+
+//        int year = 2000;
+//        if(year % 4 == 0){
+//            System.out.println(year + ": la nam nhuan");
+//        }   else {
+//            System.out.println(year + ": khong la nam nhuan");
+//        }
+
+//        Tính chỉ số bmi và đưa ra kết luận cơ thể
+//        Công thức bmi = cân nặng (kg) / chiều cao (m) * chiều cao (m)
+
+//        float height = 1.66f;
+//        float weight = 68f;
+//        float bmi = weight/(height * height);
+//
+//        System.out.println(bmi);
+//        if(bmi < 18.5){
+//            System.out.println("gay`");
+//        } else if ( bmi <= 24.9) {
+//            System.out.println("binh thuong");
+//        } else if ( bmi <= 29.9) {
+//            System.out.println("hoi beo");
+//        } else if ( bmi <= 34.9) {
+//            System.out.println("beo phi cap do 1");
+//        } else if ( bmi <= 39.9) {
+//            System.out.println("beo phi cap do 2");
+//        } else if (bmi > 40) {
+//            System.out.println("beo phi cap do 3");
+//        }
+
+
+//      Câu điều kiện switch case
+//        int month = 5;
+//        switch (month){
+//            case 2:
+//                System.out.println("Quý 1");
+//                break;
+//            case 5:
+//                System.out.println("Quý 2");
+//                break;
+//            default:
+//                System.out.println("Không rơi vào trường hợp nào");
+//                break;
+//        }
+
+
     }
 }
